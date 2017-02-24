c4642100e17d62ecc5c4cd2643cb76354a63d6c2
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-02-23 16:41:01 -0800

    clean up the code! keeping it dry and readable

--- app/js/websterapi.js
+++ app/js/websterapi.js
@@ -1,140 +1,68 @@
 // Wait for the page to load before executing JavaScript function
 document.addEventListener('DOMContentLoaded', function () {
-    function WebsterAPI(){
-      // Set variables to easily manipulate to change the word
-      this.base = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/";
-      this.word = "harm";
-      // oh wells, personal project so ill just put my key out there :p
-      // this.key = put key here
-      this.getUrl = function(){ return this.base+this.word+this.key; }
-      this.updateWord = function(word){ this.word = word; }
-      this.getWebsterWord = function(word){
-        // if user passed a value for word, update word
-        if(word){ this.updateWord(word); }
-        // XMLHttpRequest object to hit the webster api
-        var web = new XMLHttpRequest();
-        // GET request, ENDPOINT to hit
-        web.open('GET', this.getUrl());
-        // Sending the GET request to ENDPOINT
-        web.send();
-        // Prepare a function to handle the response
-        web.onreadystatechange = processRequest;
-        // Function to handle the response from the ENDPOINT
-        function processRequest(e){
-          // A 4 readyState means it is DONE and
-          // a 200 status means it finished loading the page succesfully
-          if (web.readyState == 4 && web.status == 200) {
-            // return the response from the request
-            // we made to the ENDPOINT
-            parseResponse(web.responseText);
-          }
-        }
-        function parseResponse(response){
-          // create a DOMParser object to parse the response
-          var parser = new DOMParser();
-          // returns a DOM object
-          xmlDoc = parser.parseFromString(response,"application/xml");
-          /**
-           * Structure of a success return XML is
-           *
-           * entry
-           *   -> ew
-           *   -> def
-           *     -> dt
-           *
-           * A lot has been removed from this example.
-           * See the dictionaryapi documentation for complete structure
-           */
-          displayResponse(xmlDoc.getElementsByTagName("entry"));
-        }
-        function displayResponse(xmlEntryObj){
-          // Ititarate each 'entry' from the DOM
-          for(i = 0; i < xmlEntryObj.length; i++){
-              ShowResults(xmlEntryObj[i]);
-          }
-          // Function that will do something with each entry object
-          function ShowResults(value) {
-            console.log(value.getElementsByTagName("fl")[0].textContent);
-            console.log(value.getElementsByTagName("ew")[0].textContent);
-            console.log(value.getElementsByTagName("def")[0].getElementsByTagName("dt")[0].textContent);
-          }
-        }
+  // websterWOD is an object that will get the data from webster WOD page
+  var websterWOD = {
+    wodURI: "https://www.merriam-webster.com/word-of-the-day",
+    // http is an instance of XMLHttpRequest object
+    // we are going to use http inside two functions in
+    // consecutive order. httpGET() -> processRequest()
+    http: new XMLHttpRequest(),
+    // set up, get request
+    httpGET: function () {
+      this.http.open('GET', this.wodURI);
+      this.http.onreadystatechange = this.processRequest;
+      this.http.send();
+    },
+    // process the request made by httpGET()
+    processRequest: function (e) {
+      // success at loading the page from a GET request
+      if (this.http.readyState == 4 && this.http.status == 200) {
+        var parser = new DOMParser();
+        xmlDoc = parser.parseFromString(this.http.responseText,"text/html");
+        return xmlDoc;
       }
+      // failed
+      return false;
     }
+  };
 
-    function WebsterWOD(){
-      this.url = "https://www.merriam-webster.com/word-of-the-day"
-      this.getURL = function(){
-        var web = new XMLHttpRequest();
-        // GET request, ENDPOINT to hit
-        web.open('GET', this.url);
-        //web.setRequestHeader('Access-Control-Allow-Origin', '*');
-        web.setRequestHeader('Access-Control-Allow-Headers','*');
-        // Sending the GET request to ENDPOINT
-        web.send();
-        // Prepare a function to handle the response
-        web.onreadystatechange = processRequest;
-        // Function to handle the response from the ENDPOINT
-        function processRequest(e){
-          // A 4 readyState means it is DONE and
-          // a 200 status means it finished loading the page succesfully
-          if (web.readyState == 4 && web.status == 200) {
-            // return the response from the request
-            // we made to the ENDPOINT
-            parseResponse(web.responseText);
-          }
-        }
-        function parseResponse(response){
-          // create a DOMParser object to parse the response
-          var parser = new DOMParser();
-          // returns a DOM object
-          xmlDoc = parser.parseFromString(response,"text/html");
-          var l = xmlDoc.getElementsByClassName("wod-definition-container")[0].children.length;
-          var childNodes = xmlDoc.getElementsByClassName("wod-definition-container")[0].children;
-          var count = 1;
-          var word = xmlDoc.getElementsByClassName("word-and-pronunciation")[0].firstElementChild.innerHTML;
-          var type = xmlDoc.getElementsByClassName("word-attributes")[0].firstElementChild.innerHTML;
-          document.getElementsByClassName("word")[0].innerHTML = upperCaseFirstLetter(word);
-          document.getElementsByClassName("type")[0].innerHTML = "("+type+")";
-          // document.getElementsByClassName("definition")[0].innerHTML = "Definition:";
-          // document.getElementsByClassName("example")[0].innerHTML = "Example:";
-          while(childNodes[count].textContent != 'Examples'){
-            //document.getElementsByClassName("definition-container")[0].appendChild(pElement(childNodes[count].textContent));
-            defToParagraphs(childNodes[count].textContent);
-            count++;
-          }
-          // count++;
-          // while(count < l){
-          //   document.getElementsByClassName("example-container")[0].appendChild(pElement(childNodes[count].textContent));
-          //   count++;
-          // }
-        }
+  // helpers is an object that will have functions to do outputs and string modifiers
+  var helpers = {
+    // turns the string into an array and output the definitions for the WOD
+    // the element with a class name of "definition-container" will hold the
+    // definitions
+    appendToDef: function (string) {
+      if (string[0] == ":" || string[0] == "1" ) {
+        var str = string.split(":");
+        var element = this.elementP(str[1]);
+        document.getElementsByClassName("definition-container")[0].appendChild(element);
       }
-    }
-
-    var webster = new WebsterWOD();
-    webster.getURL();
-
-    function upperCaseFirstLetter(string){
+    },
+    // returns a paragraph element containing the string parameter
+    elementP: function (string) {
+      return document.createElement("p").innerHTML = string
+    },
+    // capitalize the first letter of the string parameter
+    toUpperFirstLeter: function (string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    mainDisplay: function (xmlDoc) {
+      var count = 1;
+      var l = xmlDoc.getElementsByClassName("wod-definition-container")[0].children.length;
+      var word = xmlDoc.getElementsByClassName("word-and-pronunciation")[0].firstElementChild.innerHTML;
+      var type = xmlDoc.getElementsByClassName("word-attributes")[0].firstElementChild.innerHTML;
+      var childNodes = xmlDoc.getElementsByClassName("wod-definition-container")[0].children;
+      document.getElementsByClassName("word")[0].innerHTML = this.toUpperFirstLeter(word);
+      document.getElementsByClassName("type")[0].innerHTML = "("+type+")";
+      while(childNodes[count].textContent != 'Examples'){
+        this.appendToDef(childNodes[count].textContent);
+        count++;
+      }
     }
-    function defToParagraphs(string){
-      if(string[0] == ":"){
-        var str = string.split(":");
-        document.getElementsByClassName("definition-container")[0].appendChild(pElement(str[1]));
-      }
-      if(string[0] == "1"){
-        var str = string.split(":");
-        document.getElementsByClassName("definition-container")[0].appendChild(pElement(str[1]));
-      }
+  };
 
-      // str.forEach(function(element){
-      //   document.getElementsByClassName("definition-container")[0].appendChild(pElement(element));
-      // });
-    }
-    function pElement(text){
-      var p = document.createElement("p");
-      p.innerHTML = text;
-      return p;
-    }
+  var wod = new websterWOD();
+  var helper = new helpers();
+  var xmlDoc = wod.httpGET();
+  if (xmlDoc) helper.mainDisplay(xmlDoc);
 });
